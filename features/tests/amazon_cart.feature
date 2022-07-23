5b3f27c54e332af8cc85fb15aa62c1a8279ce6eb
HyunChoi0310 <hyun.red.cloud@gmail.com>
2022-07-23 07:57:05 -0500

    CSS_Selectors, Amazon cart empty

---
+++ features/tests/amazon_cart.feature
@@ -0,0 +1,6 @@
+# Created by thoma at 7/23/2022
+Feature: # Enter feature name here
+  # Enter feature description here
+
+  Scenario: # Enter scenario name here
+    # Enter steps here
---
+++ locators_amazon_register.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+# init driver
+driver = webdriver.Chrome(executable_path="C:/ChoiLearning/Automation/python-selenium-automation/chromedriver.exe")
+driver.maximize_window()
+# open the url
+driver.get('https://www.amazon.com/')
+# Amazon logo, By ID
+driver.find_element(By.XPATH, "//a[@href='/ref=ap_frn_logo']")
+# email field, By ID
+driver.find_element(By.ID, "//input[@id='ap_email']")
+# Continue button By ID
+driver.find_element(By.ID, "//input[@id='continue']")
+# Need help link
+driver.find_element(By.LINK_TEXT, "//span[contains(text(), 'Need help?')]")
+# Create your Amazon account button, By ID
+driver.find_element(By.ID, "//a[@id='createAccountSubmit']")
+# *Conditions of use link By XPATH
+driver.find_element(By.XPATH, "//a[@href='/gp/help/customer/display.html/ref=ap_desktop_footer_cou?ie=UTF8&nodeId=508088']")
+# *Privacy Notice link By XPATH
+driver.find_element(By.XPATH, "//a[@href='/gp/help/customer/display.html/ref=ap_desktop_footer_privacy_notice?ie=UTF8&nodeId=468496']")
+
+print('Test Passed')
+driver.quit()
+
